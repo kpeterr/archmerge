a0883ee3014ba93a4c3063d5ca0563ce687d98a2
Péter Milán Kollár <kpeterr2@gmail.com>
2018-03-05 14:36:14 +0100

    Update not-needed.sh

--- not-needed.sh
+++ not-needed.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
+set -e
+
 #################################################################
 ###############   These programs do not need me   ###############
 #################################################################
 
-sudo pacman -R atom electron geany meld
-sudo pacman -R gimp darktable gpick inkscape nomacs peek shotwell simple-scan oomox-git
-sudo pacman -R evolution filezilla hexchat transmission-gtk vivaldi vivaldi-codecs-ffmpeg-extra-bin
-sudo pacman -R plank variety xfburn
-sudo pacman -R guvcview pavucontrol
+sudo pacman -R atom electron geany meld --noconfirm
+sudo pacman -R gimp darktable gpick inkscape nomacs peek shotwell simple-scan oomox-git --noconfirm
+sudo pacman -R evolution filezilla hexchat transmission-gtk vivaldi vivaldi-codecs-ffmpeg-extra-bin --noconfirm
+sudo pacman -R plank variety xfburn --noconfirm
+sudo pacman -R guvcview pavucontrol --noconfirm
 
 echo "#################################################################"
 echo "#################     Programs are removed     ##################"
