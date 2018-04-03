830502e85e48b57e025082cea804162c4de332b4
Péter Milán Kollár <kpeterr2@gmail.com>
2018-04-03 09:25:23 +0200

    Update not-needed.sh

--- not-needed.sh
+++ not-needed.sh
@@ -6,7 +6,7 @@ set -e
 #################################################################
 
 sudo pacman -R atom electron geany meld --noconfirm
-sudo pacman -R gimp darktable gpick inkscape nomacs peek shotwell simple-scan oomox-git --noconfirm
+sudo pacman -R gimp darktable gpick inkscape nomacs peek shotwell simple-scan oomox --noconfirm
 sudo pacman -R evolution filezilla hexchat transmission-gtk vivaldi vivaldi-codecs-ffmpeg-extra-bin --noconfirm
 sudo pacman -R plank variety xfburn --noconfirm
 sudo pacman -R guvcview pavucontrol --noconfirm
